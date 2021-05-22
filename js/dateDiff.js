735325d05b316c1f2c76118c5288bafc708f2669
Jonathan Troiano <33912011+jjtroiano0272@users.noreply.github.com>
2021-05-22 19:19:45 -0400

    updating

--- js/dateDiff.js
+++ js/dateDiff.js
@@ -4,7 +4,8 @@ function roundedToFixed(_float, _digits){
 }
 
 function timeElapsed(startDate, endDate) {
-  var startDate = new Date(2017, 7, 20, 0, 0); // Feb 1, 2011
+  // Feb 1, 2011
+  var startDate = new Date(2017, 7, 20, 0, 0); 
   var endDate = new Date(); // now
   var diff = endDate.getTime() - startDate.getTime(); // now - Feb 1
   return diff / (1000 * 60 * 60 * 24)/365;
