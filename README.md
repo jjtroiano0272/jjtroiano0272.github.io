1bf5eca2e909f8b0537585216d903f9405cbbac1
Jonathan Troiano <33912011+jjtroiano0272@users.noreply.github.com>
2025-05-28 04:11:35 -0400

    New site v0.9 up!
    Just needs some real text to fill in the empty spots and it's ready to go!

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio: Jonathan Troiano
- This will contain the documentation of the process of creating this page. 
- Short, maybe with a few gifs.
---
+++ assets/arrayDataSets/skills.js
@@ -0,0 +1,141 @@
+const skills = [
+	// Programming Languages
+	{
+		name: "C#",
+		icon: "devicon-csharp-plain colored",
+		category: "programming-languages",
+	},
+	{
+		name: "C++",
+		icon: "devicon-cplusplus-plain colored",
+		category: "programming-languages",
+	},
+	{
+		name: "Java",
+		icon: "devicon-java-plain colored",
+		category: "programming-languages",
+	},
+	{
+		name: "M Power Query",
+		icon: "devicon-powershell-plain colored",
+		category: "dbms",
+	},
+	{
+		name: "Javascript",
+		icon: "devicon-javascript-plain colored",
+		category: "programming-languages",
+	},
+	{
+		name: "Typescript",
+		icon: "devicon-typescript-plain colored",
+		category: "programming-languages",
+	},
+	{
+		name: "R",
+		icon: "devicon-r-plain colored",
+		category: "programming-languages",
+	},
+	{
+		name: "Python",
+		icon: "devicon-python-plain colored",
+		category: "programming-languages",
+	},
+
+	// Web Design
+	{ name: "CSS", icon: "devicon-css3-plain colored", category: "web-dev" },
+	{
+		name: "HTML",
+		icon: "devicon-html5-plain-wordmark colored",
+		category: "web-dev",
+	},
+	{
+		name: "REST APIs",
+		icon: "devicon-powershell-plain colored",
+		category: "web-dev",
+	},
+	{
+		name: "Bootstrap",
+		icon: "devicon-bootstrap-plain colored",
+		category: "web-dev",
+	},
+	{
+		name: "Node.js",
+		icon: "devicon-nodejs-plain colored",
+		category: "web-dev",
+	},
+	{
+		name: "Responsive WebDev",
+		icon: "devicon-powershell-plain colored",
+		category: "web-dev",
+	},
+	{
+		name: "Three.js",
+		icon: "devicon-threejs-original colored",
+		category: "web-dev",
+	},
+	{
+		name: "React",
+		icon: "devicon-react-original colored",
+		category: "web-dev",
+	},
+	{
+		name: "Material UI Design",
+		icon: "devicon-materialui-plain colored",
+		category: "web-dev",
+	},
+
+	// Mobile Dev
+	{
+		name: "React Native",
+		icon: "devicon-react-original colored",
+		category: "mobile-dev",
+	},
+	{
+		name: "React Native Reanimated",
+		icon: "devicon-powershell-plain colored",
+		category: "mobile-dev",
+	},
+	{
+		name: "Expo",
+		icon: "devicon-powershell-plain colored",
+		category: "mobile-dev",
+	},
+	{
+		name: "Xcode",
+		icon: "devicon-xcode-plain colored",
+		category: "mobile-dev",
+	},
+
+	// DBMS
+	{ name: "MySQL", icon: "devicon-mysql-original colored", category: "dbms" },
+	{
+		name: "Postgres",
+		icon: "devicon-postgresql-plain colored",
+		category: "dbms",
+	},
+	{
+		name: "Firebase",
+		icon: "devicon-firebase-plain colored",
+		category: "dbms",
+	},
+	{
+		name: "Supabase",
+		icon: "devicon-supabase-plain colored",
+		category: "dbms",
+	},
+
+	// Business Analytics
+	{
+		name: "Microsoft Power BI",
+		icon: "devicon-powershell-plain colored",
+		category: "business-analytics",
+	},
+	{
+		name: "TOAD Data Point",
+		icon: "devicon-powershell-plain colored",
+		category: "business-analytics",
+	},
+
+	// Game Dev
+	{ name: "Unity", icon: "devicon-unity-plain colored", category: "game-dev" },
+];
