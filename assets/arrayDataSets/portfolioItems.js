c0495a5f7a1ea827ae51614efe0f97dae01f29d8
Jonathan Troiano <33912011+jjtroiano0272@users.noreply.github.com>
2025-05-29 13:12:18 -0400

    fixes mobile issue, video portfolio

---
+++ assets/arrayDataSets/portfolioItems.js
@@ -0,0 +1,128 @@
+const portfolioItems = [
+	{
+		imgSrc: "assets/img/portfolio/1_pelli-raw-with-icon.png",
+		name: "Pelli",
+		description:
+			"The yelp-for-hairstylists full stack social app I developed as a solo developer. Uses supabase and MySQL database integration.",
+		gitLink: "https://github.com/jjtroiano0272/Pelli",
+		appStoreLink: "https://apps.apple.com/us/app/pelli/id6737356623", // optional
+		category: "filter-mobile",
+	},
+	{
+		imgSrc: "assets/img/portfolio/2_plusOnBlock.png",
+		name: "Plus on Block",
+		description:
+			"An application built with X and Y and Z. Uses React Native Reanimated.",
+		gitLink: "https://github.com/jjtroiano0272/Pelli",
+		appStoreLink: "", // optional
+		category: "filter-mobile",
+	},
+	{
+		imgSrc: "assets/img/portfolio/3_threshold.MP4", // Possibly change this to a video preview or image fallback
+		name: "Threshold",
+		description: "Unity project developed as a demo.",
+		gitLink: "https://github.com/jjtroiano0272/Threshold",
+		appStoreLink: "",
+		category: "filter-unity",
+		isVideo: true,
+		videoEmbed: `<iframe src="https://player.vimeo.com/video/1088781918?h=6bb099b046&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479?autoplay=1&" width="320" height="180" frameborder="0" allow="autoplay=1; fullscreen; picture-in-picture; clipboard-write; encrypted-media" title="threshold clip"></iframe>`,
+	},
+	{
+		imgSrc: "assets/img/previous coding projects images/1_gwordle-light.png",
+		name: "Wordle Variant",
+		description: "A variant on Wordle.",
+		gitLink: "https://gwordle-jjt.netlify.app/",
+		appStoreLink: "",
+		category: "filter-web",
+	},
+	{
+		imgSrc:
+			"assets/img/previous coding projects images/3_studio-mockup-light.png",
+		name: "Studio Mockup",
+		description: "Site redesign mockup for a tattoo studio.",
+		gitLink: "",
+		demoLink: "https://studio-mockup.netlify.app/",
+		appStoreLink: "",
+		category: "filter-web",
+	},
+	{
+		imgSrc: "assets/img/previous coding projects images/4_vidly-app-light.png",
+		name: "SaaS - Video rental service",
+		description: "Web app for a larger-scale style of business.",
+		gitLink: "",
+		demoLink: "https://vidly-app.pages.dev/",
+		appStoreLink: "",
+		category: "filter-web",
+	},
+	{
+		category: "filter-web",
+		imgSrc: "assets/img/previous coding projects images/5_quiz.png",
+		name: "RESTful API Trivia",
+		description:
+			"Test your trivia knowledge by pulling random questions from the OpenTrivia API!",
+		demoLink: "https://react-trivia-app-jjt.netlify.app/",
+	},
+	{
+		category: "filter-web",
+		imgSrc: "assets/img/previous coding projects images/6_tenzies.png",
+		name: "Tenzies: The Dice Game",
+		description:
+			"If you've played Yahtzee, you're already familiar with how to play! Roll dice and choose which to hold on to until they all match.",
+		demoLink: "https://react-tenzies-app.netlify.app/",
+	},
+	{
+		category: "filter-web",
+		imgSrc: "assets/img/previous coding projects images/7_notes-app.png",
+		name: "Notes Markdown Editor",
+		description:
+			"An in-browser MDE that lets you write and preview text like you can in GitHub documentation.",
+		demoLink: "https://react-notes-mde.netlify.app/",
+	},
+	{
+		category: "filter-web",
+		imgSrc: "assets/img/previous coding projects images/8_ml-meme-gen.png",
+		name: "Machine Learning Meme Generator",
+		description:
+			"An app for creating your own memes. Bonus: It has a section for Machine-Learning-generated memes.",
+		demoLink: "https://react-ml-meme-generator.netlify.app/",
+	},
+	{
+		category: "filter-web",
+		imgSrc: "assets/img/previous coding projects images/9_counter.png",
+		name: "Simple Counter App",
+		description: "The first simple React app I built.",
+		demoLink: "https://counter-app.pages.dev/",
+	},
+];
+
+// const portfolioItems = [
+// 	{
+// 		title: "Pelli",
+// 		image: "assets/img/portfolio/1_pelli-raw-with-icon.png",
+// 		description:
+// 			"The yelp-for-hairstylists full stack social app I developed as a solo developer. Uses supabase and MySQL.",
+// 		github: "https://github.com/jjtroiano0272/Pelli",
+// 		storeLink: "https://apps.apple.com/us/app/pelli/id6737356623",
+// 		filter: "mobile",
+// 		appIcon: "fa-brands fa-app-store-ios fa-6x",
+// 	},
+// 	{
+// 		title: "Plus on Block",
+// 		image: "assets/img/portfolio/2_plusOnBlock.png",
+// 		description:
+// 			"An application built with X and Y and Z. Uses React Native Reanimated.",
+// 		github: "https://github.com/jjtroiano0272/Pelli",
+// 		detailsLink: "#",
+// 		filter: "mobile",
+// 	},
+// 	{
+// 		title: "Threshold",
+// 		image: "assets/img/portfolio/3_threshold.MP4",
+// 		description: "Unity project developed as a demo.",
+// 		github: "https://github.com/jjtroiano0272/Threshold",
+// 		filter: "unity",
+// 		video: `<iframe src="https://player.vimeo.com/video/1088781918?h=6bb099b046&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479?autoplay=1&"
+//       width="320" height="180" frameborder="0" allow="autoplay=1; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+//       title="threshold clip"></iframe>`,
+// 	},
+// ];
